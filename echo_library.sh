585cddfe35cbfab58f876004b7a7523dc8a2b901
dwygngrh <75960709+dwygngrh@users.noreply.github.com>
2025-09-24 10:07:18 +0700

    Update echo_library.sh

--- echo_library.sh
+++ echo_library.sh
@@ -7,12 +7,13 @@ cat << 'EOF' >> "$BASHRC"
 # >>> NetCDF & HDF5 paths (auto-generated) >>>
 export NETCDF4_DIR=/mgpfs/home/$USER/library_intel/parallel/netcdf
 export HDF5_DIR=/mgpfs/home/$USER/library_intel/parallel/hdf5
+export CURL_DIR=/mgpfs/home/$USER/library_intel/parallel/curl
 
-export PATH=$NETCDF4_DIR/bin:$HDF5_DIR/bin:$PATH
-export CPATH=$NETCDF4_DIR/include:$HDF5_DIR/include:$CPATH
-export LIBRARY_PATH=$NETCDF4_DIR/lib:$HDF5_DIR/lib:$LIBRARY_PATH
-export LD_LIBRARY_PATH=$NETCDF4_DIR/lib:$HDF5_DIR/lib:$LD_LIBRARY_PATH
-# <<< NetCDF & HDF5 paths <<<
+export PATH=$NETCDF4_DIR/bin:$HDF5_DIR/bin:$CURL_DIR/bin:$PATH
+export CPATH=$NETCDF4_DIR/include:$HDF5_DIR/include:$CURL_DIR/include:$CPATH
+export LIBRARY_PATH=$NETCDF4_DIR/lib:$HDF5_DIR/lib:$CURL_DIR/lib:$LIBRARY_PATH
+export LD_LIBRARY_PATH=$NETCDF4_DIR/lib:$HDF5_DIR/lib:$CURL_DIR/lib:$LD_LIBRARY_PATH
+# <<< NetCDF,CURL & HDF5 paths <<<
 
 EOF
 
